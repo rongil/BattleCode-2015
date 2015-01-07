1376134010e96b25cc5127d24bb0c2b56b327606
Ronald <rg8630@gmail.com>
2015-01-06 22:36:51 -0500

    Added the number of robots in each round as a broadcast from HQ

--- teams/testbot1/RobotPlayer.java
+++ teams/testbot1/RobotPlayer.java
@@ -1,9 +1,70 @@
 package testbot1;
 
 import battlecode.common.*;
+
 import java.util.Random;
 
+/**
+ * Keeping everything static to save the bytecode from the this statement
+ * instances produce!
+ */
 public class RobotPlayer {
+
+	/**************************************************************************
+	 * BROADCAST _CHANNELS
+	 * ------------------------------------------------------------------------
+	 * NOTE: Building/Units order follows directly from online documentation.
+	 * ------------------------------------------------------------------------
+	 * 0-9: Friendly Buildings
+	 * ------------------------------------------------------------------------
+	 * 10-19: Friendly Units
+	 * ------------------------------------------------------------------------
+	 * 20-29: Enemy Buildings
+	 * ------------------------------------------------------------------------
+	 * 30-39: Enemy Units
+	 *************************************************************************/
+	// Friendly Buildings Channels
+	private static final int NUM_FRIENDLY_SUPPLY_DEPOT_CHANNEL = 1;
+	private static final int NUM_FRIENDLY_MINER_FACTORY_CHANNEL = 2;
+	private static final int NUM_FRIENDLY_TECHINSTITUTE_CHANNEL = 3;
+	private static final int NUM_FRIENDLY_BARRACKS_CHANNEL = 4;
+	private static final int NUM_FRIENDLY_HELIPAD_CHANNEL = 5;
+	private static final int NUM_FRIENDLY_TRAININGFIELD_CHANNEL = 6;
+	private static final int NUM_FRIENDLY_TANKFACTORY_CHANNEL = 7;
+	private static final int NUM_FRIENDLY_AEROSPACELAB_CHANNEL = 8;
+	private static final int NUM_FRIENDLY_HANDWASHSTATION_CHANNEL = 9;
+	// Friendly Units Channels
+	private static final int NUM_FRIENDLY_BEAVERS_CHANNEL = 10;
+	private static final int NUM_FRIENDLY_MINERS_CHANNEL = 11;
+	private static final int NUM_FRIENDLY_COMPUTERS_CHANNEL = 12;
+	private static final int NUM_FRIENDLY_SOLDIERS_CHANNEL = 13;
+	private static final int NUM_FRIENDLY_BASHERS_CHANNEL = 14;
+	private static final int NUM_FRIENDLY_DRONES_CHANNEL = 15;
+	private static final int NUM_FRIENDLY_TANKS_CHANNEL = 16;
+	private static final int NUM_FRIENDLY_COMMANDERS_CHANNEL = 17;
+	private static final int NUM_FRIENDLY_LAUNCHERS_CHANNEL = 18;
+	private static final int NUM_FRIENDLY_MISSILES_CHANNEL = 19;
+	// Enemy Buildings Channels
+	private static final int NUM_ENEMY_SUPPLYDEPOT_CHANNEL = 21;
+	private static final int NUM_ENEMY_MINERFACTORY_CHANNEL = 22;
+	private static final int NUM_ENEMY_TECHINSTITUTE_CHANNEL = 23;
+	private static final int NUM_ENEMY_BARRACKS_CHANNEL = 24;
+	private static final int NUM_ENEMY_HELIPAD_CHANNEL = 25;
+	private static final int NUM_ENEMY_TRAININGFIELD_CHANNEL = 26;
+	private static final int NUM_ENEMY_TANKFACTORY_CHANNEL = 27;
+	private static final int NUM_ENEMY_AEROSPACELAB_CHANNEL = 28;
+	private static final int NUM_ENEMY_HANDWASHSTATION_CHANNEL = 29;
+	// Enemy Units Channels
+	private static final int NUM_ENEMY_BEAVERS_CHANNEL = 30;
+	private static final int NUM_ENEMY_MINERS_CHANNEL = 31;
+	private static final int NUM_ENEMY_COMPUTERS_CHANNEL = 32;
+	private static final int NUM_ENEMY_SOLDIERS_CHANNEL = 33;
+	private static final int NUM_ENEMY_BASHERS_CHANNEL = 34;
+	private static final int NUM_ENEMY_DRONES_CHANNEL = 35;
+	private static final int NUM_ENEMY_TANKS_CHANNEL = 36;
+	private static final int NUM_ENEMY_COMMANDERS_CHANNEL = 37;
+	private static final int NUM_ENEMY_LAUNCHERS_CHANNEL = 38;
+	private static final int NUM_ENEMY_MISSILES_CHANNEL = 39;
 
 	static Direction facing;
 	static Direction[] directions = { Direction.NORTH, Direction.NORTH_EAST,
@@ -27,14 +88,11 @@ public class RobotPlayer {
 			try {
 				// Random number from 0 to 1 for probabilistic decisions
 				double fate = rand.nextDouble();
-				
-				if (rc.getType() == RobotType.HQ) {
-					/*
-					 * each action requires several conditions that need to be
-					 * satisfied; otherwise, exceptions will be thrown, each of
-					 * which incurs a 500 bytecode penalty
-					 */
 
+				if (rc.getType() == RobotType.HQ) {
+					// Broadcasts how many of each robot type there currently
+					// are on the field.
+					updateUnitCounts();
 					attackEnemyZero(); /*
 										 * we can this method first before
 										 * spawning beavers because we probably
@@ -43,7 +101,10 @@ public class RobotPlayer {
 										 * addition, spawning introduces
 										 * attacking delay
 										 */
-					spawnUnit(RobotType.BEAVER);
+					// Limit number of beavers
+					if (rc.readBroadcast(NUM_FRIENDLY_BEAVERS_CHANNEL) < 10) {
+						spawnUnit(RobotType.BEAVER);
+					}
 
 				} else if (rc.getType() == RobotType.BEAVER) {
 					attackEnemyZero();
@@ -52,9 +113,9 @@ public class RobotPlayer {
 						buildUnit(RobotType.MINERFACTORY);
 					} else if (fate < 0.01) {
 						buildUnit(RobotType.SUPPLYDEPOT);
-					} else if (fate > 0.01 && fate < 0.51){
+					} else if (fate > 0.01 && fate < 0.51) {
 						buildUnit(RobotType.BARRACKS);
-					} else{
+					} else {
 						buildUnit(RobotType.TANKFACTORY);
 					}
 
@@ -132,42 +193,45 @@ public class RobotPlayer {
 	}
 
 	private static void moveAround() throws GameActionException {
-		if(rc.isCoreReady() && rc.canMove(facing)){
+		if (rc.isCoreReady() && rc.canMove(facing)) {
 			MapLocation tileInFrontLocation = rc.getLocation().add(facing);
-			TerrainTile tileInFrontTerrain = rc.senseTerrainTile(tileInFrontLocation);
+			TerrainTile tileInFrontTerrain = rc
+					.senseTerrainTile(tileInFrontLocation);
 			RobotType roboType = rc.getType();
-			
+
 			boolean tileInFrontSafe = true;
-			
-			while(tileInFrontSafe){
-				if(tileInFrontTerrain != TerrainTile.NORMAL){
-					if(!(tileInFrontTerrain == TerrainTile.VOID && (roboType == RobotType.DRONE || roboType == RobotType.MISSILE))){
+
+			while (tileInFrontSafe) {
+				if (tileInFrontTerrain != TerrainTile.NORMAL) {
+					if (!(tileInFrontTerrain == TerrainTile.VOID && (roboType == RobotType.DRONE || roboType == RobotType.MISSILE))) {
 						tileInFrontSafe = false;
 						break;
 					}
 				}
-				
-				RobotInfo[] enemyRobots = rc.senseNearbyRobots(roboType.sensorRadiusSquared, Enemy);
-				
-				for(RobotInfo r : enemyRobots){
-					if(r.location.distanceSquaredTo(tileInFrontLocation) <= r.type.attackRadiusSquared){
+
+				RobotInfo[] enemyRobots = rc.senseNearbyRobots(
+						roboType.sensorRadiusSquared, Enemy);
+
+				for (RobotInfo r : enemyRobots) {
+					if (r.location.distanceSquaredTo(tileInFrontLocation) <= r.type.attackRadiusSquared) {
 						tileInFrontSafe = false;
 						break;
 					}
 				}
-				
+
 				break;
 			}
-			
+
 			double probCutoff = tileInFrontSafe ? 0.0 : 0.75;
-			
+
 			if (rand.nextDouble() >= probCutoff) {
 				if (rand.nextDouble() < 0.5) {
 					facing = facing.rotateLeft(); // 45 degree turn
 				} else {
 					facing = facing.rotateRight();
 				}
-			}else{ // try to move in the facing direction since the tile in front is safe
+			} else { // try to move in the facing direction since the tile in
+						// front is safe
 				rc.move(facing);
 			}
 		}
@@ -218,7 +282,8 @@ public class RobotPlayer {
 		}
 	}
 
-	private static void buildUnit(RobotType roboType) throws GameActionException {
+	private static void buildUnit(RobotType roboType)
+			throws GameActionException {
 		if (rc.getTeamOre() > roboType.oreCost) {
 			Direction buildDir = getRandomDirection();
 
@@ -265,5 +330,197 @@ public class RobotPlayer {
 		default:
 			return -1;
 		}
+	}
+
+	private static void updateUnitCounts() throws GameActionException {
+		// Collect all robots into separate RobotInfo arrays.
+		RobotInfo[] myRobots = rc.senseNearbyRobots(999999, rc.getTeam());
+		RobotInfo[] enemyRobots = rc.senseNearbyRobots(999999, rc.getTeam()
+				.opponent());
+
+		// FRIENDLY
+		// Buildings
+		int numFriendlySupplyDepot = 0;
+		int numFriendlyMinerFactory = 0;
+		int numFriendlyTechInstitute = 0;
+		int numFriendlyBarracks = 0;
+		int numFriendlyHelipad = 0;
+		int numFriendlyTrainingField = 0;
+		int numFriendlyTankFactory = 0;
+		int numFriendlyAerospaceLab = 0;
+		int numFriendlyHandwashStation = 0;
+		// Other units
+		int numFriendlyBeavers = 0;
+		int numFriendlyMiners = 0;
+		int numFriendlyComputers = 0;
+		int numFriendlySoldiers = 0;
+		int numFriendlyBashers = 0;
+		int numFriendlyDrones = 0;
+		int numFriendlyTanks = 0;
+		int numFriendlyCommanders = 0;
+		int numFriendlyLaunchers = 0;
+		int numFriendlyMissiles = 0;
+
+		// ENEMY
+		// Buildings
+		int numEnemySupplyDepot = 0;
+		int numEnemyMinerFactory = 0;
+		int numEnemyTechInstitute = 0;
+		int numEnemyBarracks = 0;
+		int numEnemyHelipad = 0;
+		int numEnemyTrainingField = 0;
+		int numEnemyTankFactory = 0;
+		int numEnemyAerospaceLab = 0;
+		int numEnemyHandwashStation = 0;
+		// Other units
+		int numEnemyBeavers = 0;
+		int numEnemyMiners = 0;
+		int numEnemyComputers = 0;
+		int numEnemySoldiers = 0;
+		int numEnemyBashers = 0;
+		int numEnemyDrones = 0;
+		int numEnemyTanks = 0;
+		int numEnemyCommanders = 0;
+		int numEnemyLaunchers = 0;
+		int numEnemyMissiles = 0;
+
+		/*
+		 * Our Robots
+		 */
+		for (RobotInfo r : myRobots) {
+			RobotType type = r.type;
+			if (type == RobotType.SUPPLYDEPOT) {
+				++numFriendlySupplyDepot;
+			} else if (type == RobotType.MINERFACTORY) {
+				++numFriendlyMinerFactory;
+			} else if (type == RobotType.TECHNOLOGYINSTITUTE) {
+				++numFriendlyTechInstitute;
+			} else if (type == RobotType.BARRACKS) {
+				++numFriendlyBarracks;
+			} else if (type == RobotType.HELIPAD) {
+				++numFriendlyHelipad;
+			} else if (type == RobotType.TRAININGFIELD) {
+				++numFriendlyTrainingField;
+			} else if (type == RobotType.TANKFACTORY) {
+				++numFriendlyTankFactory;
+			} else if (type == RobotType.AEROSPACELAB) {
+				++numFriendlyAerospaceLab;
+			} else if (type == RobotType.HANDWASHSTATION) {
+				++numFriendlyHandwashStation;
+			} else if (type == RobotType.BEAVER) {
+				++numFriendlyBeavers;
+			} else if (type == RobotType.MINER) {
+				++numFriendlyMiners;
+			} else if (type == RobotType.COMPUTER) {
+				++numFriendlyComputers;
+			} else if (type == RobotType.SOLDIER) {
+				++numFriendlySoldiers;
+			} else if (type == RobotType.BASHER) {
+				++numFriendlyBashers;
+			} else if (type == RobotType.DRONE) {
+				++numFriendlyDrones;
+			} else if (type == RobotType.TANK) {
+				++numFriendlyTanks;
+			} else if (type == RobotType.COMMANDER) {
+				++numFriendlyCommanders;
+			} else if (type == RobotType.LAUNCHER) {
+				++numFriendlyLaunchers;
+			} else if (type == RobotType.MISSILE) {
+				++numFriendlyMissiles;
+			}
+		}
+		// Friendly Buildings Broadcasts
+		rc.broadcast(NUM_FRIENDLY_SUPPLY_DEPOT_CHANNEL, numFriendlySupplyDepot);
+		rc.broadcast(NUM_FRIENDLY_MINER_FACTORY_CHANNEL,
+				numFriendlyMinerFactory);
+		rc.broadcast(NUM_FRIENDLY_TECHINSTITUTE_CHANNEL,
+				numFriendlyTechInstitute);
+		rc.broadcast(NUM_FRIENDLY_BARRACKS_CHANNEL, numFriendlyBarracks);
+		rc.broadcast(NUM_FRIENDLY_HELIPAD_CHANNEL, numFriendlyHelipad);
+		rc.broadcast(NUM_FRIENDLY_TRAININGFIELD_CHANNEL,
+				numFriendlyTrainingField);
+		rc.broadcast(NUM_FRIENDLY_TANKFACTORY_CHANNEL, numFriendlyTankFactory);
+		rc.broadcast(NUM_FRIENDLY_AEROSPACELAB_CHANNEL, numFriendlyAerospaceLab);
+		rc.broadcast(NUM_FRIENDLY_HANDWASHSTATION_CHANNEL,
+				numFriendlyHandwashStation);
+		// Friendly Units Broadcasts
+		rc.broadcast(NUM_FRIENDLY_BEAVERS_CHANNEL, numFriendlyBeavers);
+		rc.broadcast(NUM_FRIENDLY_MINERS_CHANNEL, numFriendlyMiners);
+		rc.broadcast(NUM_FRIENDLY_COMPUTERS_CHANNEL, numFriendlyComputers);
+		rc.broadcast(NUM_FRIENDLY_SOLDIERS_CHANNEL, numFriendlySoldiers);
+		rc.broadcast(NUM_FRIENDLY_BASHERS_CHANNEL, numFriendlyBashers);
+		rc.broadcast(NUM_FRIENDLY_DRONES_CHANNEL, numFriendlyDrones);
+		rc.broadcast(NUM_FRIENDLY_TANKS_CHANNEL, numFriendlyTanks);
+		rc.broadcast(NUM_FRIENDLY_COMMANDERS_CHANNEL, numFriendlyCommanders);
+		rc.broadcast(NUM_FRIENDLY_LAUNCHERS_CHANNEL, numFriendlyLaunchers);
+		rc.broadcast(NUM_FRIENDLY_MISSILES_CHANNEL, numFriendlyMissiles);
+
+		/**
+		 * Enemy Robots
+		 */
+		for (RobotInfo r : enemyRobots) {
+			RobotType type = r.type;
+			if (type == RobotType.SUPPLYDEPOT) {
+				++numEnemySupplyDepot;
+			} else if (type == RobotType.MINERFACTORY) {
+				++numEnemyMinerFactory;
+			} else if (type == RobotType.TECHNOLOGYINSTITUTE) {
+				++numEnemyTechInstitute;
+			} else if (type == RobotType.BARRACKS) {
+				++numEnemyBarracks;
+			} else if (type == RobotType.HELIPAD) {
+				++numEnemyHelipad;
+			} else if (type == RobotType.TRAININGFIELD) {
+				++numEnemyTrainingField;
+			} else if (type == RobotType.TANKFACTORY) {
+				++numEnemyTankFactory;
+			} else if (type == RobotType.AEROSPACELAB) {
+				++numEnemyAerospaceLab;
+			} else if (type == RobotType.HANDWASHSTATION) {
+				++numEnemyHandwashStation;
+			} else if (type == RobotType.BEAVER) {
+				++numEnemyBeavers;
+			} else if (type == RobotType.MINER) {
+				++numEnemyMiners;
+			} else if (type == RobotType.COMPUTER) {
+				++numEnemyComputers;
+			} else if (type == RobotType.SOLDIER) {
+				++numEnemySoldiers;
+			} else if (type == RobotType.BASHER) {
+				++numEnemyBashers;
+			} else if (type == RobotType.DRONE) {
+				++numEnemyDrones;
+			} else if (type == RobotType.TANK) {
+				++numEnemyTanks;
+			} else if (type == RobotType.COMMANDER) {
+				++numEnemyCommanders;
+			} else if (type == RobotType.LAUNCHER) {
+				++numEnemyLaunchers;
+			} else if (type == RobotType.MISSILE) {
+				++numEnemyMissiles;
+			}
+		}
+
+		// Enemy Buildings Broadcasts
+		rc.broadcast(NUM_ENEMY_SUPPLYDEPOT_CHANNEL, numEnemySupplyDepot);
+		rc.broadcast(NUM_ENEMY_MINERFACTORY_CHANNEL, numEnemyMinerFactory);
+		rc.broadcast(NUM_ENEMY_TECHINSTITUTE_CHANNEL, numEnemyTechInstitute);
+		rc.broadcast(NUM_ENEMY_BARRACKS_CHANNEL, numEnemyBarracks);
+		rc.broadcast(NUM_ENEMY_HELIPAD_CHANNEL, numEnemyHelipad);
+		rc.broadcast(NUM_ENEMY_TRAININGFIELD_CHANNEL, numEnemyTrainingField);
+		rc.broadcast(NUM_ENEMY_TANKFACTORY_CHANNEL, numEnemyTankFactory);
+		rc.broadcast(NUM_ENEMY_AEROSPACELAB_CHANNEL, numEnemyAerospaceLab);
+		rc.broadcast(NUM_ENEMY_HANDWASHSTATION_CHANNEL, numEnemyHandwashStation);
+		// Enemy Units Broadcasts
+		rc.broadcast(NUM_ENEMY_BEAVERS_CHANNEL, numEnemyBeavers);
+		rc.broadcast(NUM_ENEMY_MINERS_CHANNEL, numEnemyMiners);
+		rc.broadcast(NUM_ENEMY_COMPUTERS_CHANNEL, numEnemyComputers);
+		rc.broadcast(NUM_ENEMY_SOLDIERS_CHANNEL, numEnemySoldiers);
+		rc.broadcast(NUM_ENEMY_BASHERS_CHANNEL, numEnemyBashers);
+		rc.broadcast(NUM_ENEMY_DRONES_CHANNEL, numEnemyDrones);
+		rc.broadcast(NUM_ENEMY_TANKS_CHANNEL, numEnemyTanks);
+		rc.broadcast(NUM_ENEMY_COMMANDERS_CHANNEL, numEnemyCommanders);
+		rc.broadcast(NUM_ENEMY_LAUNCHERS_CHANNEL, numEnemyLaunchers);
+		rc.broadcast(NUM_ENEMY_MISSILES_CHANNEL, numEnemyMissiles);
 	}
 }
