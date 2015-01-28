f3b4d0197e51fa5dcda8b5b0ccfeb0d5a02f0660
gfyoung <gfyoung@mit.edu>
2015-01-27 16:55:56 -0800

    Here Goes Nothing
    Took out lots of extraneous code

--- teams/droneLauncherBot3/RobotPlayer.java
+++ teams/droneLauncherBot3/RobotPlayer.java
@@ -1,8 +1,6 @@
 package droneLauncherBot3;
 
 import battlecode.common.*;
-
-import java.util.LinkedList;
 import java.util.Random;
 
 /**
@@ -36,7 +34,6 @@ public class RobotPlayer {
 	private static MapLocation[] enemyTowers;
 
 	private static int halfwayDistance;
-	private static int wholeDistanceCoefficient;
 	private static int swarmRound;
 
 	private static Direction facing;
@@ -47,9 +44,6 @@ public class RobotPlayer {
 
 	// HQ only
 	private static MapLocation lastSwarmTarget;
-
-	// Missile only
-	private static int turnsRemaining;
 
 	// Drone only
 	private static Direction patrolDirection;
@@ -98,7 +92,6 @@ public class RobotPlayer {
 			// Slightly less to avoid tower issues
 			halfwayDistance = (int) (0.45 * Math.sqrt(friendlyHQ
 					.distanceSquaredTo(enemyHQ)));
-			wholeDistanceCoefficient = friendlyHQ.distanceSquaredTo(enemyHQ) / 700;
 
 			// Drone only stuff
 			if (thisRobotType == RobotType.DRONE) {
@@ -108,10 +101,8 @@ public class RobotPlayer {
 						.rotateRight();
 			}
 
-		} else {
-			// turnsRemaining = GameConstants.MISSILE_LIFESPAN;
 		}
-
+		
 		// Method can never end or the robot is destroyed.
 		while (true) {
 			/*
@@ -650,13 +641,10 @@ public class RobotPlayer {
 	 */
 	private static void attackNearestTower() throws GameActionException {
 		// TODO: Make number counts a function of towerStrength
-		MapLocation currentLocation = rc.getLocation();
 		MapLocation targetLocation = new MapLocation(
 				rc.readBroadcast(SWARM_LOCATION_X_CHANNEL),
 				rc.readBroadcast(SWARM_LOCATION_Y_CHANNEL));
 
-		int distanceToTarget = currentLocation
-				.distanceSquaredTo(targetLocation);
 		// NOTE: Launcher has its own enemy checking and special attacking in
 		// it's case before reaching here.
 		if (thisRobotType != RobotType.LAUNCHER && rc.isWeaponReady()
@@ -720,125 +708,6 @@ public class RobotPlayer {
 		}
 		return false;
 	}
-
-	/*
-	 * ==========================================================================
-	 */
-
-	public static class SearchNode {
-		private MapLocation nodeLoc;
-		private SearchNode nodeParent;
-
-		public SearchNode(MapLocation loc, SearchNode parent) {
-			nodeLoc = loc;
-			nodeParent = parent;
-		}
-
-		public MapLocation getLoc() {
-			return nodeLoc;
-		}
-
-		public SearchNode getParent() {
-			return nodeParent;
-		}
-
-		public LinkedList<MapLocation> getPath() {
-			LinkedList<MapLocation> nodePath = new LinkedList<MapLocation>();
-			nodePath.add(0, this.getLoc());
-
-			SearchNode currentNode = this;
-
-			while (currentNode.getParent() != null) {
-				currentNode = currentNode.getParent();
-				nodePath.add(0, currentNode.getLoc());
-			}
-
-			return nodePath;
-		}
-	}
-
-	private static boolean search(MapLocation dest, boolean DFS)
-			throws GameActionException {
-		MapLocation currentLocation = rc.getLocation();
-
-		if (!reachedGoal(currentLocation, dest)) {
-			LinkedList<SearchNode> agenda = new LinkedList<SearchNode>();
-			LinkedList<MapLocation> visited = new LinkedList<MapLocation>();
-
-			agenda.add(new SearchNode(currentLocation, null));
-			visited.add(currentLocation);
-
-			SearchNode currentNode;
-
-			while (agenda.size() != 0) {
-				if (DFS) {
-					currentNode = agenda.removeLast();
-				} else {
-					currentNode = agenda.removeFirst();
-				}
-
-				MapLocation currentLoc = currentNode.getLoc();
-				MapLocation parentLoc = currentNode.getParent().getLoc();
-				Direction parentToCurrent = parentLoc.directionTo(currentLoc);
-
-				rc.broadcast(channelHashFunc(parentLoc),
-						directionToInt(parentToCurrent));
-
-				LinkedList<MapLocation> nodeLocations = getChildren(currentNode
-						.getLoc());
-
-				for (MapLocation nodeLocation : nodeLocations) {
-					SearchNode childNode = new SearchNode(nodeLocation,
-							currentNode);
-
-					if (reachedGoal(nodeLocation, dest)) {
-						Direction currentToChild = currentLoc
-								.directionTo(nodeLocation);
-						rc.broadcast(channelHashFunc(currentLoc),
-								directionToInt(currentToChild));
-						return true;
-
-					} else {
-						if (!visited.contains(nodeLocation)) {
-							visited.add(nodeLocation);
-							agenda.add(childNode);
-						}
-					}
-				}
-			}
-		}
-
-		return false;
-	}
-
-	private static boolean reachedGoal(MapLocation loc, MapLocation dest) {
-		return (loc.x == dest.x) && (loc.y == dest.y);
-	}
-
-	private static LinkedList<MapLocation> getChildren(MapLocation loc) {
-		LinkedList<MapLocation> possibleChildren = new LinkedList<MapLocation>();
-
-		for (Direction possDirection : Direction.values()) {
-			MapLocation possSquare = loc.add(possDirection);
-			TerrainTile possSquareTerrain = rc.senseTerrainTile(possSquare);
-
-			if (possSquareTerrain == TerrainTile.NORMAL) {
-				possibleChildren.add(possSquare);
-			}
-		}
-
-		return possibleChildren;
-	}
-
-	private static int channelHashFunc(MapLocation loc) {
-		int maxWidth = GameConstants.MAP_MAX_WIDTH;
-		int maxHeight = GameConstants.MAP_MAX_HEIGHT;
-		return 10000 + loc.x % maxWidth + maxWidth * loc.y % maxHeight;
-	}
-
-	/*
-	 * ==========================================================================
-	 */
 
 	/**
 	 * Gives the ratio of friend to enemy robots around a given location that
@@ -1162,23 +1031,6 @@ public class RobotPlayer {
 	}
 
 	/**
-	 * 
-	 * @param dir
-	 * @return true if the missile was able to move in the given direction,
-	 *         false otherwise
-	 * @throws GameActionException
-	 */
-	private static boolean missileMoveTowardDirection(Direction dir)
-			throws GameActionException {
-		if (rc.isCoreReady() && rc.canMove(dir)) {
-			rc.move(dir);
-			return true;
-		}
-
-		return false;
-	}
-
-	/**
 	 * Searches for enemies that have crossed the halfway point between the two
 	 * HQ's and tries to attack them; otherwise, moves around randomly.
 	 *
@@ -1475,27 +1327,6 @@ public class RobotPlayer {
 	private static int unitCountNumFriendlyTanks;
 	private static int unitCountNumFriendlyLaunchers;
 	private static int unitCountNumFriendlyMissiles;
-	// Enemy Buildings
-	private static int unitCountNumEnemySupplyDepot;
-	private static int unitCountNumEnemyMinerFactory;
-	private static int unitCountNumEnemyTechInstitute;
-	private static int unitCountNumEnemyBarracks;
-	private static int unitCountNumEnemyHelipad;
-	private static int unitCountNumEnemyTrainingField;
-	private static int unitCountNumEnemyTankFactory;
-	private static int unitCountNumEnemyAerospaceLab;
-	private static int unitCountNumEnemyHandwashStation;
-	// Enemy other units
-	private static int unitCountNumEnemyBeavers;
-	private static int unitCountNumEnemyMiners;
-	private static int unitCountNumEnemyComputers;
-	private static int unitCountNumEnemyCommanders;
-	private static int unitCountNumEnemySoldiers;
-	private static int unitCountNumEnemyBashers;
-	private static int unitCountNumEnemyDrones;
-	private static int unitCountNumEnemyTanks;
-	private static int unitCountNumEnemyLaunchers;
-	private static int unitCountNumEnemyMissiles;
 
 	/***
 	 * Collects and broadcasts the number of all unit types.
